55a61908a0430e82cdcf7f416b3c93b749586768
Adrian Javorski <adrianjavorski@gmail.com>
2020-08-07 09:47:04 -0700

    made the pwd js file, and started ls.js file

--- bash.js
+++ bash.js
@@ -1,1 +1,19 @@
-//Hola amigos
+const pwd = require('./pwd')
+const ls = require('./ls')
+process.stdout.write('prompt >');
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    switch(cmd) {
+        case 'pwd':
+            pwd(cmd)
+            break;
+        case 'ls':
+            ls(cmd)
+            break;
+        default:
+            process.stdout.write('\nERROR: NO COMMAND FOUND');
+
+    }
+    process.stdout.write('\nprompt > ');
+})
