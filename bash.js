b29d7ba7e753496ef5f29d6c8d2df566f6b9271a
Adrian Javorski <adrianjavorski@gmail.com>
2020-08-07 10:01:18 -0700

    started work on the cat command in cat.js

--- bash.js
+++ bash.js
@@ -4,12 +4,16 @@ process.stdout.write('prompt >');
 
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-    switch(cmd) {
+    let args = cmd.split(' ');
+    switch(args[0]) {
         case 'pwd':
-            pwd(cmd)
+            pwd(args[0])
             break;
         case 'ls':
-            ls(cmd)
+            ls(args[0])
+            break;
+        case 'cat':
+            cat(args);
             break;
         default:
             process.stdout.write('\nERROR: NO COMMAND FOUND');
---
+++ cat.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+module.exports = function (args) {
+        process.stdout.write(process.cwd());
+    // args[0] is command
+    // args[1] is fileName
+    }
+
+fs.read()
